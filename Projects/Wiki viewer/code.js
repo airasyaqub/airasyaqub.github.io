75bafd87f4dae8dde40fae0debd8efccd8067a2c
airasyaqub <airasyaqub@github.com>
2017-11-26 17:58:31 +0300

    new js file adding

--- Projects/Wiki viewer/code.js
+++ Projects/Wiki viewer/code.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
-	/* body... */
+  /* body... */
 
-	
-var dataList =$('#json-datalist');
-var input = $('#ajax');
+  
+
 var globalres;
-//var $ul=$('<ul></ul>');
+var keyflag=0;
 
 
-$('#searchentr').click(function(){
+$('#searchentr').click(sclick);
+$('#searchentr1').click(sclick);
 
-  for (var i = 0; i < globalres[3].length; i++) {
+
+function sclick(){
+  $('#results').html("");
+for (var i = 0; i < globalres[3].length; i++) {
        
-         var link="<div class=\"link\"><a href="+globalres[3][i]+" target=\"_blank\">"+globalres[1][i]+"</a><p>"+globalres[2][i]+"</p></div>";
+         var link="<div class=\"link col-lg-6 col-md-8 col-sm-12\"><a href="+globalres[3][i]+" target=\"_blank\">"+globalres[1][i]+"</a><p>"+globalres[2][i]+"</p></div>";
          $("#results").append(link);
   }
     
-    $('#initspg').hide(1,function(){
-
+if(keyflag==0){
+     
+     keyflag=1;
+     $('#initspg').remove();
      $("#sndspg").show();
+}
 
-    });
+}
 
-
-});
 
 $('#searchbox').keypress(function(e){
         if(e.which == 13){//Enter key pressed
@@ -31,127 +35,45 @@ $('#searchbox').keypress(function(e){
         }
     });
 
-
-
-$('#searchbox').autocomplete({
-
-	 source: function(query, result) {
-    dataList.html("");
-    //$ul.html("");
-
-$.ajax({
-
-    url:"https://en.wikipedia.org/w/api.php?list=search&search="+query.term+"&prop=info&inprop=url&action=opensearch&utf8=&format=json",
-    //url:"http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search="+query.term,
-    /*data: {
-        format: "json",
-        action: "query",
-        page: 'islam',
-        prop:"info"
-    },*/
-    method:'get',
-    dataType: 'jsonp',
-    success: function(response) {
-       console.log(response);
-       globalres=response;
-       var res=response[1];
-
-       for(var i=0;i<7;i++){
-
-          var opt=$('<option>');
-          opt.attr('value',res[i]);
-         dataList.append(opt);
-
-       }
-       
-       
-     }
-
-   });
-}
-
-});
-
-//--------------------------------------------------------
-
-var dataList1 =$('#json-datalist1');
-var input1 = $('#ajax');
-//var globalres;
-
-$('#searchentr1').click(function(){
-          
-         
-        $("#results").html("");
-  for (var i = 0; i < globalres[3].length; i++) {
-       
-         var link="<div class=\"link\"><a href="+globalres[3][i]+" target=\"_blank\">"+globalres[1][i]+"</a><p>"+globalres[2][i]+"</p></div>";
-         $("#results").append(link);
-  }
-    
-
-});
-
 $('#searchbox1').keypress(function(e){
         if(e.keyCode == 13){//Enter key pressed
-          
-          e.preventDefault();
+            e.preventDefault();
             $('#searchentr1').click();//Trigger search button click event
         }
     });
 
 
-$('#searchbox1').autocomplete({
-
+$('#searchbox').autocomplete({
    source: function(query, result) {
-    dataList.html("");
-    //$ul.html("");
 
 $.ajax({
-
     url:"https://en.wikipedia.org/w/api.php?list=search&search="+query.term+"&prop=info&inprop=url&action=opensearch&utf8=&format=json",
-    //url:"http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search="+query.term,
-    /*data: {
-        format: "json",
-        action: "query",
-        page: 'islam',
-        prop:"info"
-    },*/
     method:'get',
     dataType: 'jsonp',
     success: function(response) {
        console.log(response);
        globalres=response;
-       var res=response[1];
-
-       for(var i=0;i<7;i++){
-
-          var opt=$('<option>');
-          opt.attr('value',res[i]);
-         dataList1.append(opt);
-
-       }
-       
-       
+       result(response[1]);  
      }
-
    });
 }
-
 });
 
-/*function listgenerator(res){
+$('#searchbox1').autocomplete({
+   source: function(query, result) {
 
- //$ul=$('<ul></ul>');
-//$ul.html('');
-var sugname=res[1];
-for (var i = 0; i < 7; i++) {
-
-   $ul.append('<li>'+sugname[i]+'</li>');
-
+$.ajax({
+    url:"https://en.wikipedia.org/w/api.php?list=search&search="+query.term+"&prop=info&inprop=url&action=opensearch&utf8=&format=json",
+    method:'get',
+    dataType: 'jsonp',
+    success: function(response) {
+       console.log(response);
+       globalres=response;
+       result(response[1]);  
+     }
+   });
 }
-
-$('#searchbox').after($ul);
-}*/
+});
 
 
 });
